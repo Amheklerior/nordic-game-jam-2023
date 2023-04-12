4c9143001a9bc03f6ffdfa5c1e43e3cddb677b54
Amheklerior <andrea.amato.89@gmail.com>
2023-04-12 18:17:30 +0200

    feat: Implement a basic game state manager

---
+++ Unity.NordicGameJam.2023/Assets/Runtime/Code/Game.cs
@@ -0,0 +1,101 @@
+using System;
+using UnityEngine;
+
+public class Game
+{
+
+    #region Game States and transitions
+
+    public enum GameState
+    {
+        MAIN_MENU,
+        PLAYING,
+        PAUSED,
+        LOST,
+        WON
+    }
+
+    public GameState State { get; private set; }
+
+    public void StartGame()
+    {
+        if (State == GameState.PLAYING) return;
+        onGameStart?.Invoke();
+        State = GameState.PLAYING;
+    }
+
+    public void Pause()
+    {
+        if (State != GameState.PLAYING) return;
+        onPause?.Invoke();
+        State = GameState.PAUSED;
+    }
+
+    public void Resume()
+    {
+        if (State != GameState.PAUSED) return;
+        onResume?.Invoke();
+        State = GameState.PLAYING;
+    }
+
+    public void GameOver()
+    {
+        if (State != GameState.PLAYING) return;
+        onGameOver?.Invoke();
+        State = GameState.LOST;
+    }
+
+    public void Win()
+    {
+        if (State != GameState.PLAYING) return;
+        onGameWinning?.Invoke();
+        State = GameState.WON;
+    }
+
+    public void Quit()
+    {
+        if (State != GameState.PAUSED &&
+            State != GameState.LOST &&
+            State != GameState.WON)
+        {
+            Debug.Log(State);
+            return;
+        }
+        onGameQuit?.Invoke();
+        State = GameState.MAIN_MENU;
+    }
+
+    public void Exit()
+    {
+        if (State != GameState.MAIN_MENU) return;
+        onExit?.Invoke();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    #endregion
+
+    #region Game Events
+
+    public Action onGameStart;
+    public Action onPause;
+    public Action onResume;
+    public Action onGameOver;
+    public Action onGameWinning;
+    public Action onGameQuit;
+    public Action onExit;
+
+    #endregion
+
+    #region Singleton
+
+    private static Game _instance;
+
+    public static Game Instance => _instance ?? (_instance = new Game());
+
+    #endregion
+
+}
---
+++ Unity.NordicGameJam.2023/Assets/Runtime/Code/GameController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// TODO: Delete. This is just a helper class for a quick manual test
+public class GameController: MonoBehaviour
+{
+    public InputAction StartNewGame;
+    public InputAction Pause;
+    public InputAction Resume;
+    public InputAction GameOver;
+    public InputAction Win;
+    public InputAction Quit;
+    public InputAction Exit;
+
+    private Game _game;
+
+    private void Awake()
+    {
+        _game = new Game();
+
+        StartNewGame.performed += ctx => _game.StartGame();
+        Pause.performed += ctx => _game.Pause();
+        Resume.performed += ctx => _game.Resume();
+        GameOver.performed += ctx => _game.GameOver();
+        Win.performed += ctx => _game.Win();
+        Quit.performed += ctx => _game.Quit();
+        Exit.performed += ctx => _game.Exit();
+
+        _game.onGameStart += () => Debug.Log("New game started");
+        _game.onPause += () => Debug.Log("Game paused");
+        _game.onResume += () => Debug.Log("Game resumed");
+        _game.onGameOver += () => Debug.Log("Game Over");
+        _game.onGameWinning += () => Debug.Log("You won!");
+        _game.onGameQuit += () => Debug.Log("Back to main menu");
+        _game.onExit += () => Debug.Log("EXIT");
+    }
+
+    public void OnEnable()
+    {
+        StartNewGame.Enable();
+        Pause.Enable();
+        Resume.Enable();
+        GameOver.Enable();
+        Win.Enable();
+        Quit.Enable();
+        Exit.Enable();
+    }
+
+    public void OnDisable()
+    {
+        StartNewGame.Disable();
+        Pause.Disable();
+        Resume.Disable();
+        GameOver.Disable();
+        Win.Disable();
+        Quit.Disable();
+        Exit.Disable();
+    }
+}
