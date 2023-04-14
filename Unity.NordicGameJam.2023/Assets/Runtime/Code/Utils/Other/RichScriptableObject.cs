26f752fe724b7f7e1c9ce425e4c9221e407589cd
Amheklerior <andrea.amato.89@gmail.com>
2023-04-14 14:39:31 +0200

    feat: Copy the object pooling scripts from older projects

---
+++ Unity.NordicGameJam.2023/Assets/Runtime/Code/Utils/Other/RichScriptableObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Amheklerior.NordicGameJam2023.Utils
+{
+
+    public abstract class RichScriptableObject : ScriptableObject
+    {
+
+#if UNITY_EDITOR
+
+        [Header("Dev Settings:")]
+
+        [Tooltip(tooltip: "Activate/deactivate logs.")]
+        [SerializeField] protected bool _debugMode = false;
+
+        [SerializeField, Multiline] protected string _description;
+
+#endif
+
+    }
+}
