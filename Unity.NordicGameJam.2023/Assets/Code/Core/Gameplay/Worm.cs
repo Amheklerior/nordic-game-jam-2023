561c01d64e61f012b6e4479a56a0a4b5e49a04d9
Kristijonas Malisauskas <kristijonas.malisauskas@hutlihut.dk>
2023-04-15 17:09:12 +0200

    better snek

--- Unity.NordicGameJam.2023/Assets/Code/Core/Gameplay/Worm.cs
+++ Unity.NordicGameJam.2023/Assets/Code/Core/Gameplay/Worm.cs
@@ -1,3 +1,4 @@
+using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -15,18 +16,31 @@ public class Worm : MonoBehaviour, IFeedable
 
     public float FeedConsumptionRate;
 
+    private Vector3 startPoint;
+    private const float PATH_AMPLITUDE = 2.0f;
+    private const float PATH_WAVE_LENGTH = 4.0f;
+    private const float FORWARD_DISTANCE = 0.3f;
+
     private void Awake()
     {
+        startPoint = transform.position;
         _transform = transform;
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private Vector3 TargetMovePoint()
+    {
+        float evaluateDistance = transform.position.x - startPoint.x + FORWARD_DISTANCE;
+        var   targetY          = Mathf.Sin(evaluateDistance * Mathf.PI / PATH_WAVE_LENGTH) * PATH_AMPLITUDE + startPoint.y;
+        return new Vector3(transform.position.x + FORWARD_DISTANCE, targetY, transform.position.z);
+    }
+
     public void FixedUpdate()
     {
-        _rigidbody.velocity = Vector3.right * (BaseVelocity * FeedToVelocityCurve.Evaluate(currentFeed / MaxFeed));
-
+        Vector3 targetVector = TargetMovePoint() - transform.position;
+        _rigidbody.velocity = targetVector.normalized * (BaseVelocity * FeedToVelocityCurve.Evaluate(currentFeed / MaxFeed));
+        _rigidbody.SetRotation(Quaternion.Euler(0.0f, 0.0f, Mathf.Atan2(targetVector.y, targetVector.x) * Mathf.Rad2Deg));
         currentFeed -= FeedConsumptionRate * Time.fixedDeltaTime;
-
         currentFeed = Mathf.Clamp(currentFeed, 0.0f, MaxFeed);
     }
 
@@ -35,6 +49,12 @@ public class Worm : MonoBehaviour, IFeedable
         currentFeed += res.FoodAmount;
         currentFeed = Mathf.Clamp(currentFeed, 0.0f, MaxFeed);
         Destroy(res.gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(TargetMovePoint(), 0.5f);
     }
 
     [Button]
